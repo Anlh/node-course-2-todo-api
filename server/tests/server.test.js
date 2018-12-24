a1beb7e4829018564430ffb10e0a5187c5bfb3b0
Anlh <heldern19@gmail.com>
2018-12-24 15:48:01 +0000

    Add tests for GET /api/v1/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,18 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todosFixture = [
+    { text: 'First text todo' },
+    { text: 'Second text todo' }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({})
+        .then(() => Todo.insertMany(todosFixture))
+        .then(() => done());
 });
 
-describe('POST /todos', () => {
+describe('POST /api/v1/todos', () => {
     it('should create a new todo', done => {
         const text = 'Test todo text';
 
@@ -24,7 +31,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then(todos => {
+                Todo.find({ text }).then(todos => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +50,19 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then(todos => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(err => done(err));
             })
     });
-})
+});
+
+describe('GET /api/v1/todos', () => {
+    it('Should get all todos', done => {
+        request(app)
+            .get('/api/v1/todos')
+            .expect(200)
+            .expect(res => expect(res.body.todos.length).toBe(2))
+            .end(done);
+    });
+});
