8425bbf81cd863d9b24cb84109a02062b40c4dda
Anlh <heldern19@gmail.com>
2018-12-24 17:50:39 +0000

    Add test cases for GET /api/v1/todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todosFixture = [
-    { text: 'First text todo' },
-    { text: 'Second text todo' }
+    { text: 'First text todo', _id: new ObjectID() },
+    { text: 'Second text todo', _id: new ObjectID() }
 ];
 
 beforeEach((done) => {
@@ -66,3 +67,29 @@ describe('GET /api/v1/todos', () => {
             .end(done);
     });
 });
+
+describe('GET /api/v1/todos/:id', () => {
+    it('Should return todo doc', done => {
+        request(app)
+            .get(`/api/v1/todos/${todosFixture[0]._id.toHexString()}`)
+            .expect(200)
+            .expect(res => expect(res.body.todo.text).toBe(todosFixture[0].text))
+            .end(done);
+    });
+
+    // Exercice: Return 404 if todo not found
+    it('Should return 404 if a real todo id not found', done => {
+        request(app)
+            .get(`/api/v1/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    // Exercice: Invalid id return 404
+    it('Should return 404 for non-object ids', done => {
+        request(app)
+            .get(`/api/v1/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+})
