07a57d1d6f9f97636266ff84fa16776e7e705c23
Anlh <heldern19@gmail.com>
2018-12-24 16:46:41 +0000

    Add GET /api/v1/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const { mongoose } = require('mongoose');
+const { ObjectID } = require('mongodb');
+
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
@@ -21,9 +22,25 @@ app.post('/api/v1/todos', (req, res) => {
 });
 
 app.get('/api/v1/todos', (req, res) => {
-    Todo.find().then(todos => res.send({todos})),
-    err => res.status(400).send(err);
+    Todo.find().then(todos => res.send({ todos })),
+        err => res.status(400).send(err);
 });
+
+// Exercice fetching an individual todo item
+app.get('/api/v1/todos/:id', (req, res) => {
+    const todoId = req.params.id;
+
+    if (!ObjectID.isValid(todoId)) {
+        return res.status(404).send()
+    }
+
+    Todo.findById(todoId).then(todo => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        return res.send({ message: 'Todo found', todo });
+    }).catch(err => res.status(404).send());
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
